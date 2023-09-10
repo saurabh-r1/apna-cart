a6f3e923019cd3c890bbfef9956435468fdd8533
saurabh-r1 <sidlock2@gmail.com>
2023-09-10 05:55:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AddItem from "./components/AddItem";
 
 function App() {
+  const products = [
+    {
+      price: 9999,
+      name: "Redmi Note 10S Max",
+      quantity: 0,
+    },
+    {
+      price: 99999,
+      name: "Iphone 10S Max",
+      quantity: 0,
+    },
+  ];
+
+  const [productList, setProductList] = useState(products);
+
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+
+    newTotalAmount += newProductList[index].price;
+    setTotalAmount(newTotalAmount);
+    newProductList[index].quantity++;
+    setProductList(newProductList);
+  };
+
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+
+    if (newProductList[index].quantity > 0) {
+      newProductList[index].quantity--;
+      newTotalAmount -= newProductList[index].price;
+    }
+    setProductList(newProductList);
+    setTotalAmount(newTotalAmount);
+  };
+
+  const resetQuantity = () => {
+    let newProductList = [...productList];
+    newProductList.map((products) => {
+      products.quantity = 0;
+    });
+    setProductList(newProductList);
+    setTotalAmount(0);
+  };
+
+  const removeItem = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+
+    newTotalAmount =
+      newProductList[index].quantity * newProductList[index].price;
+    newProductList.splice(index, 1);
+
+    setProductList(newProductList);
+    setTotalAmount(newTotalAmount);
+  };
+
+  const addItem = (name, price) => {
+    let newProductList = [...products];
+    newProductList.push({
+      name: name,
+      price: price,
+      quantity: 0,
+    });
+    setProductList(newProductList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <main className="container mt-5">
+        <AddItem addItem={addItem} />
+
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          removeItem={removeItem}
+        />
+      </main>
+
+      <Footer totalAmount={totalAmount} resetQuantity={resetQuantity} />
+    </>
   );
 }
 
